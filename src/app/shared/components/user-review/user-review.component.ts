93340941e8c1cc49627054ce652d8655948ac647
Max-Lev <maxlevtov@gmail.com>
2025-08-02 19:09:15 +0300

    rating conditional starts

--- src/app/shared/components/user-review/user-review.component.ts
+++ src/app/shared/components/user-review/user-review.component.ts
@@ -1,3 +1,4 @@
+import { NgClass } from '@angular/common';
 import { Component, effect, input } from '@angular/core';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 import { Product, Review } from 'src/app/core/models/products';
@@ -6,7 +7,8 @@ import { StrictUser } from 'src/app/core/models/user.model';
 @Component({
   selector: 'app-user-review',
   imports: [
-    AngularSvgIconModule
+    AngularSvgIconModule,
+    NgClass
   ],
   templateUrl: './user-review.component.html',
   styleUrl: './user-review.component.css'
@@ -23,7 +25,15 @@ export class UserReviewComponent {
   constructor() { 
     effect(()=>{
       this.product().reviews.sort((a,b)=>b.rating-a.rating);
-    })
+    });
+  
+  }
+
+  getRatingCategory(rating: number): 'poor' | 'fair' | 'good' | 'excellent' {
+    if (rating < 2) return 'poor';
+    if (rating < 3) return 'fair';
+    if (rating < 4) return 'good';
+    return 'excellent';
   }
 
 }
